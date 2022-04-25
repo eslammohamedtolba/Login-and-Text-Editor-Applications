dacb50f0119f3c872d679df1a4b4ed5abe737e9c
eslam <91751277+eslammohamedtolba@users.noreply.github.com>
2022-04-25 19:41:37 +0200

    eslam added this files

---
+++ login application/main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "eslam" << endl;
+    return 0;
+}
---
+++ text editor/main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "eslammohamed" << endl;
+    return 0;
+}
