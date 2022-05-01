3ac8d0c4b5e43139daf2603b58c373871f31152c
eslam <91751277+eslammohamedtolba@users.noreply.github.com>
2022-05-01 17:56:53 +0200

    eslam added his part in this application

--- login application/main.cpp
+++ login application/main.cpp
@@ -1,4 +1,4 @@
-// FCAI – Programming 1 – 2022 - Assignment 4
+// FCAI â€“ Programming 1 â€“ 2022 - Assignment 4
 // Program Name: xxxxxx.cpp
 // Last Modification Date: xx/xx/xxxx
 // Author1 and ID and Group: xxxxx xxxxx
@@ -8,20 +8,97 @@
 // Purpose:..........
 #include <bits/stdc++.h>
 using namespace std;
+//------------------------------------------------------------------------------------------------------------------------------------
+fstream fileinformation,storingfile;string filename="user informations.txt";
+struct infuser
+{
+    string ID,email,password,username,mobile;
+};
+map<string,infuser>mapinfusers;
+//-------------------------------------------------------------------------------------------------------------------------------------
+void fillmapinfusers()
+{
+    fileinformation.open(filename,ios::in);string line1,line2,line3,line4,line5;
+    while(!fileinformation.eof())
+    {
+        getline(fileinformation,line1);
+        getline(fileinformation,line2);
+        getline(fileinformation,line3);
+        getline(fileinformation,line4);
+        getline(fileinformation,line5);
+        mapinfusers[line1] = {line1,line2,line3,line4,line5};
+    }
+    fileinformation.close();
+}
+//------------------------------------------------------------------------------------------------------------------------------------
+void fillfile(infuser user)
+{
+    fileinformation.open(filename,ios::app);
+    fileinformation<<user.ID<<endl;
+    fileinformation<<user.email<<endl;
+    fileinformation<<user.password<<endl;
+    fileinformation<<user.username<<endl;
+    fileinformation<<user.mobile<<endl;
+    fileinformation.close();
+}
+//-------------------------------------------------------------------------------------------------------------------------------------
 void to_register()
 {
-
+    //your code...........
+    fillfile(user);fillmapinfusers();
+    cout<<"the registration completed successfully"<<endl;
 }
 void to_login()
 {
-
+    
 }
-void to_change_password()
+void to_change_password(infuser &user)
 {
+    to_login(user);string oldpassword=user.password,newpassword;
+    while(!checkpassword(newpassword)){};
+    fileinformation.open(filename,ios::in);string line1,line2,line3,line4,line5;
+    storingfile.open("forstoringdata.txt",ios::out);
+    while(!fileinformation.eof())
+    {
+        getline(fileinformation,line1);
+        getline(fileinformation,line2);
+        getline(fileinformation,line3);
+        getline(fileinformation,line4);
+        getline(fileinformation,line5);
+        storingfile<<line1<<endl;
+        storingfile<<line2<<endl;
+        if(line3==oldpassword){
+            storingfile<<newpassword<<endl;
+            storingfile<<line4<<endl;
+            storingfile<<line5<<endl;
+            break;
+        }
+        else{
+            storingfile<<line3<<endl;
+            storingfile<<line4<<endl;
+            storingfile<<line5<<endl;
+        }
+    }
+    storingfile.close();
+    fileinformation.close();
 
+    fileinformation.open(filename,ios::out);
+    storingfile.open("forstoringdata.txt",ios::in);
+    while(!storingfile.eof())
+    {
+        getline(storingfile,line1);
+        fileinformation<<line1<<endl;
+    }
+    storingfile.close();
+    fileinformation.close();
+    remove("forstoringdata.txt");
+    cout<<"the changing for your password is done "<<endl;
 }
+//----------------------------------------------------------------------------------------------------------------------------------------
 int main()
 {
+    infuser user;
+    fillmapinfusers();
     int option;bool state=true;
     while(state){
         cout <<"what do you want to do from this menu\n"
@@ -32,13 +109,13 @@ int main()
             ">>>> ";
         cin>>option;
         if(option==1){
-            to_register();
+            to_register(user);
         }
         else if(option==2){
-            to_login();
+            to_login(user);
         }
         else if(option==3){
-            to_change_password();
+            to_change_password(user);
         }
         else if(option==4){
             state=false;
