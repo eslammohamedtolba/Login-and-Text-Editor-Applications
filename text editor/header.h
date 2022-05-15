5b7521a7ed11f14bab4753a4e9eb5af25b1fd3d3
eslam <91751277+eslammohamedtolba@users.noreply.github.com>
2022-05-15 03:27:19 +0200

    eslam added some modifications

--- text editor/header.h
+++ text editor/header.h
@@ -1,15 +1,6 @@
 #ifndef HEADER_H_INCLUDED
 #define HEADER_H_INCLUDED
 
-// FCAI � Programming 1 � 2022 - Assignment 4
-// Program Name: Text Editor Application
-// Last Modification Date: xx/xx/xxxx
-// Author1 and ID and Group: Eslam mohamed abdel azim ali, 20211013, group A
-// Author2 and ID and Group: Sama Ahmed Saeed, 20210163, group A
-// Author3 and ID and Group: Omar Rabea Shaban Bayoumi, 20210593, group A
-// Teaching Assistant: Eng.Afaf Abelmonem
-// Purpose:this application applys some modifications on files that the user enters
-
 #include <iostream>
 #include <sstream>
 #include <fstream>
--- text editor/main.cpp
+++ text editor/main.cpp
@@ -1,3 +1,11 @@
+// FCAI � Programming 1 � 2022 - Assignment 4
+// Program Name: Text Editor Application
+// Last Modification Date: 15/5/2022
+// Author1 and ID and Group: Eslam mohamed abdel azim ali, 20211013, group A
+// Author2 and ID and Group: Sama Ahmed Saeed, 20210163, group A
+// Author3 and ID and Group: Omar Rabea Shaban Bayoumi, 20210593, group A
+// Teaching Assistant: Eng.Afaf Abelmonem
+// Purpose:this application applys some modifications on files that the user enters
 #include "header.h"
 int main()
 {
